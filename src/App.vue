9a06490362b058b5405bd070c8f6336c91abe49c
ayophanz <ayophanz18@gmail.com>
2024-04-24 11:22:26 +0800

    organization sidebar - in progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="flex h-full flex-col">
     <div class="mx-auto h-full flex w-full max-w-7xl items-start">
-      <aside class="sticky h-full w-44 shrink-0 block py-1 px-2 border-r border-gray-200">
+
+      <aside :class="compSideMenuHideToggle ? 'w-44 block' : 'w-0 hidden'" class="sticky h-full shrink-0 py-1 px-2 border-r border-gray-200">
+        <div class="flex justify-end mb-2">
+            <a href="javascript:;" @click="sideMenuHide()">
+              <ArrowLeftIcon class="h-5 w-5 flex-none text-gray-400" aria-hidden="true"/>
+            </a>
+        </div>
         <ul role="list" class="divide-y divide-gray-100">
-          <li v-for="(menu, key) in menus" :key="key" class="relative flex justify-between gap-x-6 py-5">
+          <li v-for="(menu, key) in menus" :key="key" class="relative flex justify-between gap-x-6 py-2">
             <div class="flex min-w-0 gap-x-4">
               <div class="min-w-0 flex-auto">
                 <p class="text-sm font-semibold leading-6 text-gray-900">
@@ -31,7 +37,10 @@
 </template>
 
 <script setup lang="ts">
-import { ChevronRightIcon } from '@heroicons/vue/solid';
+import { ref, computed } from 'vue';
+import { ChevronRightIcon, ArrowLeftIcon } from '@heroicons/vue/solid';
+
+const sideMenuHideToggle = ref(true);
 
 const menus = [
   {
@@ -69,4 +78,10 @@ const menus = [
     ]
   }
 ]
+
+const sideMenuHide = () => {
+  sideMenuHideToggle.value = !sideMenuHideToggle.value;
+};
+
+const compSideMenuHideToggle = computed(() => sideMenuHideToggle.value);
 </script>
